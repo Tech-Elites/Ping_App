5bb8bbe2830770b362211e94cb45526e8ad63007
harshva89 <harshvaswani89@gmail.com>
2021-08-15 02:11:06 +0530

    home list

--- app/src/main/java/com/example/ping/pingspage.java
+++ app/src/main/java/com/example/ping/pingspage.java
@@ -11,6 +11,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -60,15 +73,67 @@ public class pingspage extends Fragment {
     }
 
     Button companions, pingbacks;
+    CustomAdaptorHomeList customAdaptor;
+    ArrayList<PingRequest> arrayList;
+    ListView listView;
+    FirebaseUser user;
+    ProgressBar progressBar;
 
     void onCompanionClick(){
+        progressBar.setVisibility(View.VISIBLE);
         companions.setBackgroundColor(Color.parseColor("#3C7BFB"));
         pingbacks.setBackgroundColor(Color.parseColor("#FFFFFF"));
+        arrayList=new ArrayList<>();
+        DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReference().child(user.getUid()).child("ping_from_c");
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot snapshot1:snapshot.getChildren())
+                {
+                    PingRequest temp;
+                    temp=snapshot1.getValue(PingRequest.class);
+                    arrayList.add(temp);
+                }
+                customAdaptor=new CustomAdaptorHomeList(getContext(),arrayList);
+                listView.setAdapter(customAdaptor);
+                progressBar.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
     }
 
     void onPingbacksClick(){
+        progressBar.setVisibility(View.VISIBLE);
         pingbacks.setBackgroundColor(Color.parseColor("#3C7BFB"));
         companions.setBackgroundColor(Color.parseColor("#FFFFFF"));
+        arrayList=new ArrayList<>();
+        DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReference().child(user.getUid()).child("ping_back_from_s");
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot snapshot1:snapshot.getChildren())
+                {
+                    PingRequest temp;
+                    temp=snapshot1.getValue(PingRequest.class);
+
+                    arrayList.add(temp);
+                }
+                customAdaptor=new CustomAdaptorHomeList(getContext(),arrayList);
+                listView.setAdapter(customAdaptor);
+                progressBar.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     @Override
@@ -76,6 +141,9 @@ public class pingspage extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         companions=getView().findViewById(R.id.homeCompanions);
         pingbacks=getView().findViewById(R.id.homePingBacks);
+        listView=getView().findViewById(R.id.homePageListView);
+        user= FirebaseAuth.getInstance().getCurrentUser();
+        progressBar=getView().findViewById(R.id.homePageProgress);
         companions.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -92,6 +160,7 @@ public class pingspage extends Fragment {
                 onPingbacksClick();
             }
         });
+        onCompanionClick();
     }
 
     @Override
