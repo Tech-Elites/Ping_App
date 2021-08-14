26d37203e8be1827979a0bc43f4ae83abce5e65e
harshva89 <harshvaswani89@gmail.com>
2021-08-14 17:38:25 +0530

    navbar and logout

--- app/src/main/java/com/example/ping/LandingPage.java
+++ app/src/main/java/com/example/ping/LandingPage.java
@@ -2,15 +2,21 @@ package com.example.ping;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
+import android.app.ActivityOptions;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -18,6 +24,8 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class LandingPage extends AppCompatActivity {
+
+    Toolbar tb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,28 +35,40 @@ public class LandingPage extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this,  R.id.fragment);
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
 
-    }
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater menuInflater=getMenuInflater();
-        menuInflater.inflate(R.menu.dropdownmenu,menu);
-        return super.onCreateOptionsMenu(menu);
+
+        tb=findViewById(R.id.myToolbar);
+        setSupportActionBar(tb);
 
     }
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        super.onOptionsItemSelected(item);
-        switch(item.getItemId())
-        {
-            case R.id.SignOut:
-                Toast.makeText(this, "Signout", Toast.LENGTH_SHORT).show();
-                FirebaseAuth.getInstance().signOut();
-                finish();
-                startActivity(new Intent(this,LoginPage.class));
-                return  true;
 
-            default:
-                return false;
-        }
+
+    public void GoToNotifications(View view) {
+        startActivity(new Intent(this,NotificationPage.class));
+    }
+
+    public void LogOut(View view) {
+        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which){
+                    case DialogInterface.BUTTON_POSITIVE:
+                        FirebaseAuth.getInstance().signOut();
+                        Intent i = new Intent(LandingPage.this, LoginPage.class);
+                        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(i, ActivityOptions.makeSceneTransitionAnimation(LandingPage.this).toBundle());
+
+                        break;
+
+                    case DialogInterface.BUTTON_NEGATIVE:
+
+                        break;
+                }
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Are your sure you want to log out?").setPositiveButton("Yes", dialogClickListener)
+                .setNegativeButton("No", dialogClickListener).show();
 
     }
 }
